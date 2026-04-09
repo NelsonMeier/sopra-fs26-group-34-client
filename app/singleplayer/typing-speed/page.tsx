079b0836f88c9113a1ef08edbc08a66ff40f26b2
Jonas.dkf <jonas.dekallasfischer@gmail.com>
2026-04-09 08:14:45 -0300

    started with typing game layout

--- app/singleplayer/typing-speed/page.tsx
+++ app/singleplayer/typing-speed/page.tsx
@@ -5,5 +5,21 @@ import { User } from "@/types/user";
 import { useParams, useRouter } from "next/navigation";
 
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
+const TypingSpeed: React.FC = () => {
+    const rounter = useRouter();
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [gameState, setGameState] = useState<'idle' | 'playing' | 'finished'>('idle');
+    const [quote, setQuote] = useState<string>('');
+    const [userInput, setUserInput] = useState<string>('');
+
+    const [timeElapsed, setTimeElapsed] = useState<number>(0);
+    const [wpm, setWpm] = useState<number>(0);
+    const [startTime, setStartTime] = useState<number | null>(null);
+
+
+
+    
+}
