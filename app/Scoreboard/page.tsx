621dc97e298012cd4aa9cf7cc64ed1ef53ca7244
nelsonmeier <nelsonlinus.meier@uzh.ch>
2026-04-13 18:14:50 +0200

    Created Folder and File for Scoreboared page

---
+++ app/Scoreboard/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+const Scoreboard: React.FC = () => {
+
+
+    return(
+        <div style={{
+            minHeight: '100vh', 
+            display: 'flex',
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center", 
+            backgroundColor: '#77B8D2', 
+            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', 
+            overflow: 'hidden', 
+            outline: '3px black solid',
+            outlineOffset: '-1.50px'}}>
+
+        </div>
+    );
+};
+
+export default Scoreboard;
