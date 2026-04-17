587d91289e71d57893e1bba98c3cdfade0c0235c
Zara <zara.ahmed@uzh.ch>
2026-04-17 13:01:34 +0200

    change prod url

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -7,7 +7,7 @@ import { isProduction } from "@/utils/environment";
  */
 export function getApiDomain(): string {
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
-    "https://sopra-fs26-group-34-server.appspot.com";
+    "https://sopra-fs26-group-34-server.oa.r.appspot.com";
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
 }
